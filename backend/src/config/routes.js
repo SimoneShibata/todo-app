7507edca2c2f967061595726f1b3fb87057c184e
SimoneShibata <simone_shibata@hotmail.com>
2017-09-15 00:03:10 -0300

    front-end

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 module.exports = function(server) {
     // API ROUTES
     const router = express.Router()
-    server.use('api', router)
+    server.use('/api', router)
 
     //  TODO ROUTES
     const todoService = require('../api/todo/todoService')
